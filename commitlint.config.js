931146a59d4800a90a354678e0b8f65f79f5a114
cuibaoxu <1571068777@qq.com>
2022-02-08 14:11:32 +0800

    fix(main): 提交验证

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: cuibx
+ * @Date: 2022-02-08 14:04:24
+ * @LastEditTime: 2022-02-08 14:04:25
+ * @Description: 提交验证
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
